d7786a3c5db574b5ec280a11a3f8575e0360b422
EmiRoberti77 <emiliano.roberti@accenture.com>
2024-02-29 07:47:18 +0000

    JWT express token

---
+++ src/routes/login.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const getUser = async (username) => {
+  return { userId: '123', password: 'password', username };
+};
+const sk = process.env.SECRET_KEY;
+
+module.exports = (app) => {
+  app.post('/login', async (req, res) => {
+    const { password, username } = req.body;
+    const user = await getUser(username);
+
+    if (password !== user.password) {
+      return res.status(403).json({
+        error: 'invalid user',
+      });
+    }
+    delete user.password;
+
+    const token = jwt.sign(user, sk, {
+      expiresIn: '1h',
+    });
+
+    res.cookie('token', token, {
+      httpOnly: true,
+    });
+
+    return res.redirect('/welcome.html');
+  });
+};
---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const loginRoute = require('./routes/login');
+const port = 8000;
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(cookieParser());
+
+loginRoute(app);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.get('/welcome', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/welcome.html'));
+});
+
+app.listen(port, () => {
+  console.info('started', port, new Date().toISOString());
+});
